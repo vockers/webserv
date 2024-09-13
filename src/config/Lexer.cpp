00d31af1781d36a2dab71ce4e536a64a1315b09c
Vincent Ockers <vincentbockers@gmail.com>
2024-09-13 12:17:33 +0200

    Add lexer for config

---
+++ src/config/Lexer.cpp
@@ -0,0 +1,52 @@
+#include "config/Lexer.hpp"
+
+namespace config
+{
+using Token = Lexer::Token;
+
+Lexer::Lexer(const std::string& input)
+    : _input(input)
+    , _current(_input.begin())
+{
+}
+
+Token Lexer::next_token()
+{
+    skip_whitespaces();
+
+    if (_current == _input.end()) {
+        return {Token::Type::NONE, ""};
+    }
+
+    switch (*_current) {
+    case ';':
+        ++_current;
+        return {Token::Type::RULE_END, {}};
+    case '{':
+        ++_current;
+        return {Token::Type::BLOCK_START, {}};
+    case '}':
+        ++_current;
+        return {Token::Type::BLOCK_END, {}};
+    default:
+        std::string word;
+        while (_current != _input.end() && !is_delimiter(*_current)) {
+            word += *_current;
+            ++_current;
+        }
+        return {Token::Type::WORD, word};
+    }
+}
+
+void Lexer::skip_whitespaces()
+{
+    while (_current != _input.end() && std::isspace(*_current)) {
+        ++_current;
+    }
+}
+
+bool Lexer::is_delimiter(char c)
+{
+    return std::isspace(c) || c == '{' || c == '}';
+}
+}
