6ca4812a69f3d435322a3e8371633e9f003ce37d
Vincent Ockers <vincentbockers@gmail.com>
2024-10-15 12:02:31 +0200

    Add docs

--- include/utils/std_utils.hpp
+++ include/utils/std_utils.hpp
@@ -4,6 +4,13 @@
 
 namespace webserv::utils
 {
+/// @brief Check if a container contains a value.
+///
+/// @tparam Container The type of the container.
+/// @tparam T The type of the value.
+/// @param container The container to check.
+/// @param value The value to check.
+/// @return True if the container contains the value, false otherwise.
 template <typename Container, typename T>
 bool contains(const Container& container, const T& value)
 {
